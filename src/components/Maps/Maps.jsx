20f30f58474f8c10f37b1ec9f1546559f55dea82
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-07-21 15:57:48 -0500

    user inputs address, pin gets dropped

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,44 +1,82 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader, } from '@react-google-maps/api';
-
-const containerStyle = {
-  width: '100%',
-  height: '500px'
-  
-};
+import React from "react";
+import {useEffect} from "react"
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import {useDispatch, useSelector} from 'react-redux';
 
 
-const marker = {
- lat: 44.9537,
-  lng:  -93.091301
-}
 
-const center = {
-  lat: 44.9537,
-  lng:  -93.091301
-};
+
+
+ 
+
+
 
 function Maps() {
-  const { isLoaded } = useJsApiLoader({
-    id: '28012bfa74f851d9',
-    googleMapsApiKey: "AIzaSyBfq3NpI06FkLbrezLE42LwKtxP3yEQHFU"
-  })
+  const geolocation = useSelector((store)=> store.geolocation);
+  
+  const containerStyle = {
+    width: "100%",
+    height: "500px",
+  };
 
+  const center = {
+    lat: 44.9537,
+    lng: -93.091301,
+  };
+  
+  
+  const marker = {
+    lat:Number(geolocation.lat),
+    lng: Number(geolocation.lng),
+  };
+  // const icon = {
+//   url:"racket.png",
+//   // anchor: new google.maps.Point(17, 46),
+
+//   // scaledSize: new google.maps.Size(50, 50)
+// }
+
+// const scale = {
+//   scale: 1.5
+// }
+  
+  // const markers = [
+  //   [
+  //     44.945655,
+  //     -93.12167319999999
+  //   ]
+  // ] // end markers array
   
 
+
+  const { isLoaded } = useJsApiLoader({
+    id: "c8acd79f4ac60dee",
+    googleMapsApiKey: "AIzaSyCAIMhJjtNU1n983ZoVZVj9fgp9LyVxih8",
+  });
+
+  
 
   return isLoaded ? (
     
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={12}
-        marker={{lat: 44.9537,
-          lng:  -93.091301}}
-      >
-        <></>
-      </GoogleMap>
-  ) : <></>
+    <GoogleMap
+    
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={12}
+      clickable={true}
+      // id={c8acd79f4ac60dee}
+    >
+      <Marker position={marker}
+      // icon={icon}
+      ></Marker>
+      <></>
+    </GoogleMap>
+
+    
+  ) : (
+    <>LOADING</>
+  );
+  
 }
 
-export default React.memo(Maps)
+export default React.memo(Maps);
