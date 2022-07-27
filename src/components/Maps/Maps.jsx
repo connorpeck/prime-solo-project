1b8fdbf30dffa169af517cd14107abee6bbfa625
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-07-27 15:22:01 -0500

    icons added to infoWindow

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 import {
   GoogleMap,
   useJsApiLoader,
@@ -9,14 +9,15 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import ReviewForm from "../ReviewForm/ReviewForm";
 import { useParams } from "react-router-dom";
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-
-
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import StarIcon from '@mui/icons-material/Star';
+import DeleteIcon from '@mui/icons-material/Delete';
 // import {GoogleMapsReact} from 'google-map-react'
 const center = {
   lat: 44.9537,
-  lng: -93.091301
+  lng: -93.091301,
 };
 
 // const icon = {
@@ -32,29 +33,31 @@ function Maps() {
   const pins = useSelector((store) => store.pins);
   const [selected, setSelected] = useState(null);
   const [rating, setRating] = useState(null);
-  const [review, setReview] = useState('');
+  const [review, setReview] = useState("");
+  const [show, setShow] = useState(true);
 
+  const toggleShow = () => {
+    setShow(!show);
+    console.log("in toggle show");
+  }; // end toggleShow
 
-
-
-  function addReview () {
-    console.log('in add review', review);
-    dispatch({type: 'SET_REVIEW', payload: selected.id, review});
+  function addReview() {
+    console.log("in add review", review);
+    dispatch({ type: "SET_REVIEW", payload: selected.id, review });
     dispatch({ type: "FETCH_PINS" });
-  
+
     // dispatch({type: 'ADD_REVIEW', payload:review});
     // dispatch({type:'SET_REVIEW', payload:review})
   }
 
-  function addRating () {
-    console.log('in add rating', rating);
-    dispatch({type: 'SET_RATING', payload: selected.id, rating});
+  function addRating() {
+    console.log("in add rating", rating);
+    dispatch({ type: "SET_RATING", payload: selected.id, rating });
     dispatch({ type: "FETCH_PINS" });
-  
+
     // dispatch({type: 'ADD_REVIEW', payload:review});
     // dispatch({type:'SET_REVIEW', payload:review})
   }
-
 
   useEffect(() => {
     dispatch({ type: "FETCH_PINS" });
@@ -68,15 +71,12 @@ function Maps() {
     height: "500px",
   };
 
-  
-
   // const id = ["72d068a3c1025ca3"]
 
   const mapRef = useRef();
   const onMapLoad = (map) => {
     mapRef.current = map;
-  }
-
+  };
 
   // const center = useEffect(()=> ({lat: 44.9537,
   //   lng: -93.091301}), []);/// trying to not recenter on pin click
@@ -84,92 +84,86 @@ function Maps() {
     console.log(selected.id);
     console.log("in deleteCourt");
     dispatch({ type: "DELETE_COURT", payload: selected.id });
-  
   }
-  
- 
 
   // const scale = {
   //   scale: 1.5
   // }
 
-
-
-const customMapStyle = [
+  const customMapStyle = [
     {
-        featureType: "poi",
-        elementType: "geometry",
-        stylers: [
-            {
-                color: "#eeeeee",
-            },
-        ],
+      featureType: "poi",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#eeeeee",
+        },
+      ],
     },
     {
-        featureType: "poi",
-        elementType: "labels.text",
-        stylers: [
-            {
-                visibility: "off",
-            },
-        ],
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
     },
     {
-        featureType: "water",
-        elementType: "geometry.fill",
-        stylers: [
-            {
-                color: "#d1e7ff",
-            },
-        ],
+      featureType: "water",
+      elementType: "geometry.fill",
+      stylers: [
+        {
+          color: "#d1e7ff",
+        },
+      ],
     },
     {
       featureType: "landscape",
       elementType: "geometry.fill",
       stylers: [
-          {
-              color: "#c5e2cf",
-          },
-      ],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry.fill",
-    stylers: [
         {
-            color: "#feffa8",
+          color: "#c5e2cf",
         },
-    ],
-},
-{
-  featureType: "road.local",
-  elementType: "geometry.fill",
-  stylers: [
-      {
+      ],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.fill",
+      stylers: [
+        {
+          color: "#feffa8",
+        },
+      ],
+    },
+    {
+      featureType: "road.local",
+      elementType: "geometry.fill",
+      stylers: [
+        {
           color: "#e0f000",
-      },
-  ],
-},
-{
-  featureType: "road.local.drivable",
-  elementType: "all",
-  stylers: [
-      {
-        visibility: "off",
-      },
-  ],
-},
-{
-  featureType: "road.arterial",
-  elementType: "all",
-  stylers: [
-      {
-        color: "#fff799",
-      },
-  ],
-},
-];
-   
+        },
+      ],
+    },
+    {
+      featureType: "road.local.drivable",
+      elementType: "all",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "road.arterial",
+      elementType: "all",
+      stylers: [
+        {
+          color: "#fff799",
+        },
+      ],
+    },
+  ];
 
   const { isLoaded } = useJsApiLoader({
     id: "72d068a3c1025ca3",
@@ -179,71 +173,79 @@ const customMapStyle = [
   return isLoaded ? (
     <div>
       <Paper elevation={6}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={12}
-        // onLoad={onMapLoad}
-        minZoom={-3}
-        options={{
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={12}
+          // onLoad={onMapLoad}
+          minZoom={-3}
+          options={{
             styles: customMapStyle,
-        }}
-        
+          }}
 
-        // id={c8acd79f4ac60dee}
-      >
-        {pins.map((marker) => (
-          <Marker
-            onClick={() => {
-              setSelected(null);
-              setSelected(marker);
-            }}
-            position={{ lat: Number(marker.lat), lng: Number(marker.lng) }}
-            
-            
-            icon={{
-              url:"racket.png",
-              // anchor: new google.maps.Point(17, 46),
-            animation: google.maps.Animation.BOUNCE,
-              scaledSize: new google.maps.Size(60, 60)}}
-
-          ></Marker>
-        ))}
-        {selected ? (
-          <InfoWindow
-            position={{ lat: Number(selected.lat), lng: Number(selected.lng) }}
-            onCloseClick={() => {
-              setSelected(null);
-            }}
-          >
-            <div>
-              <p>Tennis Court Test</p>
-              <div className="reviewForm">
-                <h2>
-                  Court Address: {selected.address}
-                </h2>
-                <h2>
-                  Review: {selected.review}<br /><input onChange={(event) => setReview(event.target.value)}></input>
-                  
-                  <Button onClick={addReview}>Update Review</Button>
-                </h2>
-                <h2>
-                  Rating: {selected.rating} <select onChange={(event) => setRating(event.target.value)}>
-                    <option value='1'>1</option>
-                    <option value='2'>2</option>
-                    <option value='3'>3</option>
-                    <option value='4'>4</option>
-                    <option value='5'>5</option>
-                    </select><Button onClick={addRating}>Update Rating</Button> </h2>
-              </div>
+          // id={c8acd79f4ac60dee}
+        >
+          {pins.map((marker) => (
+            <Marker
+              onClick={() => {
+                setSelected(null);
+                setSelected(marker);
+              }}
+              position={{ lat: Number(marker.lat), lng: Number(marker.lng) }}
+              icon={{
+                url: "racket.png",
+                // anchor: new google.maps.Point(17, 46),
+                animation: google.maps.Animation.BOUNCE,
+                scaledSize: new google.maps.Size(60, 60),
+              }}
+            ></Marker>
+          ))}
+          {selected ? (
+            <InfoWindow
+              position={{
+                lat: Number(selected.lat),
+                lng: Number(selected.lng),
+              }}
+              onCloseClick={() => {
+                setSelected(null);
+              }}
+            >
               <div>
-              <Button onClick={deleteCourt}>Delete</Button>
+                <p>Tennis Court Test</p>
+                <div className="reviewForm">
+                  <h2>Court Address: {selected.address}</h2>
+                  <h2>
+                    
+                    Review: {selected.review}
+                    <br />
+                    <input
+                      onChange={(event) => setReview(event.target.value)}
+                    ></input>
+                    <Button onClick={addReview}>Update Review</Button>
+                    <BorderColorIcon onClick={toggleShow} className='toggleReview'/>
+                    
+                  </h2>
+                  <h2>
+                    Rating: {selected.rating}
+                    <select onChange={(event) => setRating(event.target.value)}>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
+                    <Button onClick={addRating}>Update Rating</Button>
+                    <StarIcon onClick={toggleShow} className='toggleRating'/>
+                  </h2>
+                </div>
+                <div>
+                  <Button endIcon={<DeleteIcon/>} variant="contained" onClick={deleteCourt}>Delete</Button>
+                </div>
               </div>
-            </div >
-          </InfoWindow>
-        ) : null}
-        <></>
-      </GoogleMap>
+            </InfoWindow>
+          ) : null}
+          <></>
+        </GoogleMap>
       </Paper>
     </div>
   ) : (
