e44ed2b2002c15ef31d110c96d8ada2ac8b26ac8
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-08-26 10:57:30 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -8,3 +8,34 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "profile" (
+	"id" SERIAL PRIMARY KEY,
+	"first_name" VARCHAR,
+	"last_name" VARCHAR,
+	"bio" VARCHAR,
+	"hand" VARCHAR,
+	"game_type" VARCHAR,
+	"gender" VARCHAR
+);
+
+CREATE TABLE "profile" (
+	"id" SERIAL PRIMARY KEY,
+	"first_name" VARCHAR,
+	"last_name" VARCHAR,
+	"bio" VARCHAR,
+	"hand" VARCHAR,
+	"game_type" VARCHAR,
+	"gender" VARCHAR,
+	"user_id" INTEGER
+);
+
+CREATE TABLE "geolocation" (
+    "id" SERIAL PRIMARY KEY,
+    "lat" varchar,
+    "lng" varchar,
+    "address" varchar,
+    "review" varchar,
+    "rating" varchar,
+    "user_id" varchar
+);
